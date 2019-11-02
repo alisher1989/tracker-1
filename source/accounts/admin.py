24569e0f1f79d0819fdd5053a3930f8715c983b2
alisher <shera.rashiduulu@gmail.com>
2019-11-03 00:16:08 +0200

    Filled admin.py

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Profile, Team
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    fields = ['avatar', 'about_user', 'git_profile']
+
+
+class UserProfileAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'user_key', 'project_key', 'started_at', 'ended_at']
+    fields = ['user_key', 'project_key', 'started_at', 'ended_at']
+    labels = {'user_key': 'User', 'project_key': 'Project', 'started_at': 'Started at', 'ended_at': 'Ended at'}
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserProfileAdmin)
+admin.site.register(Team, TeamAdmin)
