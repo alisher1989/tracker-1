d1c9f6f1cc035af5a1db9eddf9303278db851456
alisher <shera.rashiduulu@gmail.com>
2019-11-05 18:26:28 +0200

    Модифицирована форма создания задачи так, чтобы в качестве автора автоматически указывался текущий пользователь. Добавлены поля в админку

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -3,11 +3,11 @@ from webapp.models import Task, Type, Status, Project
 
 
 class TodolistAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'summary', 'description', 'status', 'type', 'created_at', 'project']
+    list_display = ['pk', 'summary', 'description', 'status', 'type', 'created_at', 'project', 'created_by', 'assigned_to']
     list_filter = ['summary']
     list_display_links = ['pk', 'description']
     search_fields = ['summary', 'description']
-    fields = ['summary', 'description', 'type', 'status', 'created_at', 'project']
+    fields = ['summary', 'description', 'type', 'status', 'created_at', 'project', 'created_by', 'assigned_to']
     readonly_fields = ['created_at']
 
 
@@ -15,6 +15,6 @@ admin.site.register(Task, TodolistAdmin)
 admin.site.register(Type)
 admin.site.register(Status)
 admin.site.register(Project)
-from django.contrib import admin
+
 
 # Register your models here.
