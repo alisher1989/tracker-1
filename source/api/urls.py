f0b7c441ee85c26f53e187bc14fea55e7559a226
alisher <shera.rashiduulu@gmail.com>
2019-12-03 14:27:40 +0600

    Created urls.py in app api, and included to main urls.py

---
+++ source/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+from api.views import ProjectViewSet
+
+router = routers.DefaultRouter()
+router.register(r'projects', ProjectViewSet)
+
+app_name = 'api'
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
