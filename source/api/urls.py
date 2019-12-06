a8c1752eaf2d958f3f89c58e3c7cd1911707ce49
alisher <shera.rashiduulu@gmail.com>
2019-12-06 16:18:42 +0600

    Added url path of logout

--- source/api/urls.py
+++ source/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 
-from api.views import ProjectViewSet, TaskViewSet
+from api.views import ProjectViewSet, TaskViewSet, LogoutView
 
 router = routers.DefaultRouter()
 router.register(r'projects', ProjectViewSet)
