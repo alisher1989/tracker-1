c8c98cc3ab86081a91164608fe58f3d94a2dabda
alisher <shera.rashiduulu@gmail.com>
2019-12-06 16:57:27 +0600

    Added permissions to create and update projects and tasks

--- source/api/views.py
+++ source/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, DjangoModelPermissionsOrAnonReadOnly, DjangoModelPermissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -17,11 +17,14 @@ class LogoutView(APIView):
             user.auth_token.delete()
         return Response({'status': 'OK'})
 
+
 class ProjectViewSet(viewsets.ModelViewSet):
+    permission_classes = [DjangoModelPermissions]
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
 
 
 class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
