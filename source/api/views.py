e0131fb1a1d3322e49443a64536f3740b9a4736c
alisher <shera.rashiduulu@gmail.com>
2019-12-06 16:18:06 +0600

    Created class LogoutView in views.py of api

--- source/api/views.py
+++ source/api/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from webapp.models import Project, Task
 from .serializers import ProjectSerializer, TaskSerializer
 
+
+class LogoutView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        if user.is_authenticated:
+            user.auth_token.delete()
+        return Response({'status': 'OK'})
 
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
