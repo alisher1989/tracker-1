cd03b5820f95bacba353192955e80b492f6ee587
alisher <shera.rashiduulu@gmail.com>
2019-12-06 16:15:29 +0600

    Added login to urls of api

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+
 from api.views import ProjectViewSet, TaskViewSet
 
 router = routers.DefaultRouter()
@@ -12,5 +14,7 @@ app_name = 'api'
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', obtain_auth_token, name='obtain_token_auth'),
+
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
